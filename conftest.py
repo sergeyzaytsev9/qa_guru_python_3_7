236feb2c028e608c76cc38a8a62e93495d693886
sergeyzaytsev9 <70372468+sergeyzaytsev9@users.noreply.github.com>
2022-12-19 13:32:23 +0300

    Исправил фикстуру для установки разрешения экрана Исправил версию selene

--- conftest.py
+++ conftest.py
@@ -4,4 +4,4 @@ from selene.support.shared import browser
 @pytest.fixture()
 def set_window_size():
     browser.config.window_width = 1920
-    browser.config.window_width = 1080
+    browser.config.window_height = 1080
